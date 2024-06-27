7d69ef6c05c9a8db24cf5e594ffba4ad1f69fefa
Lans Kaba <lanskabaofficial@gmail.com>
2024-06-27 14:34:09 +0000

    first commit

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -3,13 +3,13 @@ const path = require("path");
 
 // Multer config
 module.exports = multer({
-	storage: multer.diskStorage({}),
-	fileFilter: (req, file, cb) => {
-		let ext = path.extname(file.originalname);
-		if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".gif") {
-			cb(new Error("File type is not supported"), false);
-			return;
-		}
-		cb(null, true);
-	},
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".gif") {
+      cb(new Error("File type is not supported"), false);
+      return;
+    }
+    cb(null, true);
+  },
 });
