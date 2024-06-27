8da1dd48d9f279c4f0189d3225173627a0428210
Lans Kaba <lanskabaofficial@gmail.com>
2024-06-27 15:44:19 +0000

    delete admin panel

--- src/app/api/ansys/post/[id]/route.js
+++ src/app/api/ansys/post/[id]/route.js
@@ -3,12 +3,12 @@ import { connect } from "../../../../../config/dbConfig";
 import AnsysPost from "../../../../../models/AnsysPost";
 
 export async function GET(req, { params }) {
-	await connect();
-	try {
-		const catId = params.id;
-		const post = await AnsysPost.findById(catId);
-		return NextResponse.json(post);
-	} catch (err) {
-		return NextResponse.json(err);
-	}
+  await connect();
+  try {
+    const catId = params.id;
+    const post = await AnsysPost.findById(catId);
+    return NextResponse.json(post);
+  } catch (err) {
+    return NextResponse.json(err);
+  }
 }
