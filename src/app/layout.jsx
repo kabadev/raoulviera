36f9215011692aa4587721af9aecb8bbbd4736fb
Lans Kaba <lanskabaofficial@gmail.com>
2024-07-06 20:38:09 +0000

    delete admin panel

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,14 @@
 import "./globals.css";
 
 export const metadata = {
-	title: "Mr Raoul Vierra",
-	description: "My resume",
+  title: "Mr Raoul Vierra",
+  description: "My resume",
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
